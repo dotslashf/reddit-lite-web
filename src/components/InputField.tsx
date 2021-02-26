8275c82495d0fd9d50d45bd1e4875a16a3dd4088
fadhlu <fadhlurahman.i@gmail.com>
2021-02-26 13:00:45 +0700

    add form register

---
+++ src/components/InputField.tsx
@@ -0,0 +1,38 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+import { useField } from 'formik';
+import React, { InputHTMLAttributes } from 'react';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+};
+
+// double !!
+// return string to boolean
+// '' = false
+// 'this is a string' = true
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
