c65523c1bb236692ea05f0f70c2a9b75f83e4dc6
fadhlu <fadhlurahman.i@gmail.com>
2021-02-27 20:01:38 +0700

    add ssr for urql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import { Navbar } from '../components/Navbar';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { withUrqlClient } from 'next-urql';
+import { usePostsQuery } from '../generated/graphql';
 
-const Index = () => (
-  <>
-    <Navbar />
-    <div>hello world</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <Navbar />
+      <div>hello world</div>
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map(p => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
